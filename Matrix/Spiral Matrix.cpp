7529fc34f1900bb71ac9d1c24469030ba4021802
Harshul Negi <harshul.negi19@gmail.com>
2025-09-07 08:15:47 +0530

    Add Spiral Matrix - C++ solution with explanation

---
+++ Matrix/Spiral Matrix.cpp
@@ -0,0 +1,59 @@
+/*
+📘 Problem: Spiral Matrix (LeetCode 54)
+
+📝 Description:
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+🔹 Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+🔹 Example 2:
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+🔒 Constraints:
+- m == matrix.length
+- n == matrix[i].length
+- 1 <= m, n <= 10
+- -100 <= matrix[i][j] <= 100
+*/
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        if (matrix.empty()) return ans;
+
+        int top = 0, bottom = matrix.size() - 1;
+        int left = 0, right = matrix[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from Left to Right
+            for (int j = left; j <= right; j++)
+                ans.push_back(matrix[top][j]);
+            top++;
+
+            // Traverse from Top to Bottom
+            for (int i = top; i <= bottom; i++)
+                ans.push_back(matrix[i][right]);
+            right--;
+
+            // Traverse from Right to Left
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--)
+                    ans.push_back(matrix[bottom][j]);
+                bottom--;
+            }
+
+            // Traverse from Bottom to Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    ans.push_back(matrix[i][left]);
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
